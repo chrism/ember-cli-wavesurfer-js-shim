6eae6748120193902b6e36de7d530ec96e3baf8a
Chris Masters <chrismasters@gmail.com>
2016-08-18 12:38:45 +0200

    Example showing working plugin

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ module.exports = {
     this._super.included(app);
 
     app.import(app.bowerDirectory + '/wavesurfer.js/dist/wavesurfer.min.js');
+
+    var plugins = app.project.config(process.env.EMBER_ENV)['APP']['wavesurferPlugins'];
+
+    if (Array.isArray(plugins)) {
+      plugins.forEach( plugin => {
+        app.import(app.bowerDirectory + `/wavesurfer.js/dist/plugin/wavesurfer.${plugin}.min.js`);
+      });
+    }
+
     app.import('vendor/ember-cli-wavesurfer-js-shim.js');
     app.import('vendor/demo_audio.mp3');
   }
---
+++ tests/dummy/app/components/wavesurfer-regions.js
@@ -0,0 +1,59 @@
+import Ember from 'ember';
+import layout from '../templates/components/wavesurfer-regions';
+
+import Wavesurfer from 'wavesurfer';
+
+export default Ember.Component.extend({
+  layout,
+
+  loadWavesurferPromise() {
+    const src = this.get('src');
+
+    let promise = new Ember.RSVP.Promise((resolve, reject) => {
+      const wavesurfer = Wavesurfer.create({
+          container: '#waveform-regions',
+          waveColor: 'red',
+          progressColor: 'purple',
+          normalize: true,
+          barWidth: 2
+      });
+
+      wavesurfer.load(src);
+
+      wavesurfer.on('ready', () => {
+        Ember.run(() => {
+          resolve(wavesurfer);
+        });
+      });
+
+      wavesurfer.on('error', error => {
+        Ember.run(() => {
+          reject(error);
+        });
+      });
+
+    });
+
+    return promise;
+  },
+
+  loadWavesurfer() {
+    this.loadWavesurferPromise()
+    .then( wavesurfer => {
+      Ember.Logger.log('waveform loaded');
+
+      wavesurfer.addRegion({
+        start: 1, // time in seconds
+        end: 4, // time in seconds
+        color: 'rgba(255, 0, 0, 0.1)'
+      });
+    })
+    .catch( error => {
+      Ember.Logger.log('waveform error : ', error);
+    });
+  },
+
+  didInsertElement() {
+    Ember.run.scheduleOnce('afterRender', this, this.loadWavesurfer);
+  }
+});
